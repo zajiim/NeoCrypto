b3fbeaa7f05b8fb76aa79ac204c9963a4c6e0661
sajims95 <sajims95@gmail.com>
2022-12-11 21:54:52 +0530

    Created Retrofit instance and called api to display top currencies market on horizontal recyclerview

--- app/src/main/java/com/neon/cryptoapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/neon/cryptoapp/api/RetrofitInstance.kt
@@ -1,4 +1,15 @@
 package com.neon.cryptoapp.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitInstance {
+
+    private const val BASE_URL = "https://api.coinmarketcap.com/"
+
+    fun getInstance(): Retrofit = Retrofit.Builder().baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+
 }
